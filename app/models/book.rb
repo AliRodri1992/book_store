8e069c84f43ef7c4efa33185391d38d9d96e5ad4
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-08-04 18:30:54 -0500

    Funcionalidad de crear libro
    Se desarrolla la funcionalidad para crear libro y sus respectivas validaciones.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,16 @@
 class Book < ApplicationRecord
+  paginates_per 10
+
+  validates :title, presence: true
+  validates :title, :length => { :minimum => 5 }, allow_blank: true
+  validates :author, :length => { :minimum => 5 }, allow_blank: true
+  validates :year, presence: true
+
+  # validates_format_of :title, with: /^([a-z A-ZñáéíóúÑÁÉÍÓÚ[0-9]\s]+)$/, multiline: true, :allow_blank => true
+
+  def self.get_index_columns
+    exclude_columns = %w[id created_at updated_at]
+    columns = Book.column_names
+    columns - exclude_columns
+  end
 end
