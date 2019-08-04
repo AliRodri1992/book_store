04710a332b25c5e5ae9ff036df2d2438e0328f78
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-08-04 15:34:46 -0500

    Generación automatica de archivos de testing
    Se añade codigo al application.rb para que genere de manera automatica, los factories en su carpeta correspondiente.

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,16 @@ module StoreBook
     config.time_zone = 'Mexico City'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :es
+    config.generators do |g|
+      g.test_framework :rspec,
+                       :fixtures => true,
+                       :view_specs => false,
+                       :helper_specs => false,
+                       :routing_specs => false,
+                       :controller_specs => true,
+                       :request_specs => true
+      g.fixture_replacement :factory_bot, :dir => "spec/factories"
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
