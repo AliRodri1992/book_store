5078ffaf63df7e87e272944d7bcd3012a0f222c0
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-08-04 16:26:24 -0500

    Migración
    Se genera la migración para la creación de la tabla libros y su respectivo modelo.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_163036) do
+ActiveRecord::Schema.define(version: 2019_08_04_205656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "codename"
