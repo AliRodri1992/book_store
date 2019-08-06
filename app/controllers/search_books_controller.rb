8059d634a0aeea660a50cb277910ac66b737e8c4
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-08-06 15:56:06 -0500

    Finalización de método de busqueda

--- app/controllers/search_books_controller.rb
+++ app/controllers/search_books_controller.rb
@@ -1,10 +1,10 @@
 class SearchBooksController < ApplicationController
   def search
     unless params[:term].blank?
-      @books = Book.search(params[:term])
+      @books = Book.search(params[:term]).page(params[:page]).per(10)
       puts "Size: #{@books.size}"
     else
-      @books = Book.all
+      @books = Book.all.page(params[:page]).per(9)
       puts "Size: #{@books.size}"
     end
   end
