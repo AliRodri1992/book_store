d0838114d92700d9ef60ae89ee24dca967c533ed
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2019-08-06 08:03:08 -0500

    Migración
    Se genera la tabla intermedia de libros y tiendas.

---
+++ app/models/asign_book.rb
@@ -0,0 +1,4 @@
+class AsignBook < ApplicationRecord
+  belongs_to :store
+  belongs_to :book
+end
---
+++ spec/models/asign_book_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe AsignBook, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
